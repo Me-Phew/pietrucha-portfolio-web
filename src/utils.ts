27e08abdbeda9b41cb5f8cf55545c1935d9466ee
Me-Phew <basmateusz@wp.pl>
2023-07-02 00:53:11 +0200

    Improve creator message

--- src/utils.ts
+++ src/utils.ts
@@ -13,16 +13,13 @@ const getFileSize = (numberOfBytes: number | undefined) => {
 };
 
 const printCreatorMessage = () => {
-  if (navigator.userAgent.toLowerCase().includes('chrome')) {
-    const n = [
-      `
-    %c Made with \u2665 by MePhew %c https://github.com/Me-Phew`,
-      'color: #080808; background: #69f24a; padding:5px 0;',
-      'color: #fff; background: #1c1c1c; padding:5px 0;',
-    ];
-    window.console.log.apply(console, n);
-  } else
-    window.console && window.console.log('Made with \u2665 by MePhew - https://github.com/Me-Phew');
+  const n = [
+    `
+  %c Made with \u2665 by MePhew %c https://github.com/Me-Phew`,
+    'color: #080808; background: #69f24a; padding:5px 0;',
+    'color: #fff; background: #1c1c1c; padding:5px 0;',
+  ];
+  window.console.log.apply(console, n);
 };
 
 const logWarningPropValidationFailed = (message: string) => {
