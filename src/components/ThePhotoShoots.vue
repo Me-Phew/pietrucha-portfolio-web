266ca3772bdae3f0ec701edb8c23df65907c4fdc
Me-Phew <basmateusz@wp.pl>
2023-07-04 22:17:00 +0200

    Create photo shoots section placeholder

---
+++ src/components/ThePhotoShoots.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const photoShootData = reactive({
+  date: new Date().toLocaleDateString(),
+  name: 'No name',
+});
+</script>
+
+<template>
+  <div class="photo-shoots-wrapper">
+    <div class="photo-shoots">
+      <div class="photo-shoot"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.photo-shoots-wrapper {
+  padding: 1rem 0;
+
+  .photo-shoots {
+    .photo-shoot {
+    }
+  }
+}
+</style>
