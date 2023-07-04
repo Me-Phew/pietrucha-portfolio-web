55a426036b72a34629a0f0e0fe438d25ebad9f11
Me-Phew <basmateusz@wp.pl>
2023-07-04 18:22:17 +0200

    Greatly improve styling and functionality

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-import { PhX } from '@phosphor-icons/vue';
+import IconButton, { TooltipPosition } from '@/components/IconButton.vue';
+import { PhCornersOut, PhFrameCorners, PhX } from '@phosphor-icons/vue';
+import {
+  useFullscreen,
+  useMouse,
+  useMouseInElement,
+  useMousePressed,
+  watchThrottled,
+} from '@vueuse/core';
 import { useFocusTrap } from '@vueuse/integrations/useFocusTrap';
 import { NScrollbar } from 'naive-ui';
-import { nextTick, ref, watch } from 'vue';
+import { computed, nextTick, ref, watch } from 'vue';
 
 const props = defineProps({
   open: {
@@ -36,14 +44,87 @@ const closing = ref(false);
 
 const modalOpen = ref(props.open);
 
+const fullscreenWrapper = ref<HTMLElement | null>(null);
+
+const { isFullscreen, exit, toggle } = useFullscreen(fullscreenWrapper);
+
+const toggleFullscreen = () => {
+  toggle();
+};
+
+const exitFullscreen = () => {
+  if (isFullscreen.value) {
+    exit();
+  }
+};
+
+const fullscreenBtn = ref<HTMLElement | null>(null);
+const expandBtn = ref<HTMLElement | null>(null);
+const closeBtn = ref<HTMLElement | null>(null);
+
+const { isOutside: isOutsideFullscreenBtn } = useMouseInElement(fullscreenBtn);
+const { isOutside: isOutsideExpandBtn } = useMouseInElement(expandBtn);
+const { isOutside: isOutsideCloseBtn } = useMouseInElement(closeBtn);
+
+const mouseIsOverControls = computed(() => {
+  return !isOutsideFullscreenBtn.value || !isOutsideExpandBtn.value || !isOutsideCloseBtn.value;
+});
+
+const { pressed } = useMousePressed();
+
+const controlsHidden = ref<boolean>(false);
+
+const { x, y } = useMouse();
+
+const hideControls = () => {
+  controlsHidden.value = true;
+};
+
+let hideControlsTimerId: number;
+
+const setHideControlsTimeout = () => {
+  hideControlsTimerId = setTimeout(() => {
+    if (!mouseIsOverControls.value && !pressed.value) {
+      hideControls();
+    }
+  }, 500);
+};
+
+const removeHideControlsTimeout = () => {
+  clearTimeout(hideControlsTimerId);
+};
+
+const showControls = () => {
+  removeHideControlsTimeout();
+  controlsHidden.value = false;
+  setHideControlsTimeout();
+};
+
+let cancelXWatch: Function;
+let cancelYWatch: Function;
+
 const closeModal = () => {
+  exitFullscreen();
+
   if (!closing.value) {
     deactivateModalFocus();
     closing.value = true;
     emit('update:open', false);
+
+    if (props.fullscreen) {
+      document.removeEventListener('mouseup', setHideControlsTimeout);
+      cancelXWatch();
+      cancelYWatch();
+      removeHideControlsTimeout();
+    }
+
     setTimeout(() => {
       modalOpen.value = false;
       closing.value = false;
+
+      if (props.fullscreen) {
+        controlsHidden.value = false;
+      }
     }, 400);
   }
 };
@@ -76,6 +157,15 @@ const openModal = async () => {
 
   await nextTick();
   activateModalFocus();
+
+  if (props.fullscreen) {
+    document.addEventListener('mouseup', setHideControlsTimeout);
+
+    cancelXWatch = watchThrottled(x, showControls, { throttle: 500 });
+    cancelYWatch = watchThrottled(y, showControls, { throttle: 500 });
+
+    setHideControlsTimeout();
+  }
 };
 
 watch(
@@ -88,6 +178,40 @@ watch(
     }
   },
 );
+
+enum ObjectFit {
+  contain = 'contain',
+  cover = 'cover',
+}
+
+const objectFit = ref<ObjectFit>(ObjectFit.contain);
+
+const toggleObjectFit = () => {
+  switch (objectFit.value) {
+    case ObjectFit.contain: {
+      objectFit.value = ObjectFit.cover;
+      break;
+    }
+    case ObjectFit.cover: {
+      objectFit.value = ObjectFit.contain;
+      break;
+    }
+  }
+};
+
+const expandBtnText = computed(() => {
+  switch (objectFit.value) {
+    case ObjectFit.contain: {
+      return 'Powiększ';
+    }
+    case ObjectFit.cover: {
+      return 'Dopasuj rozmiar';
+    }
+    default: {
+      return '';
+    }
+  }
+});
 </script>
 
 <template>
@@ -113,32 +237,92 @@ watch(
           :class="{ shake: shake }"
           tabindex="0"
         >
-          <div class="header">
-            <div class="title">
-              <slot name="title" />
+          <template v-if="fullscreen">
+            <div
+              class="fullscreen-wrapper"
+              ref="fullscreenWrapper"
+            >
+              <IconButton
+                class="close-btn"
+                :class="{ hidden: controlsHidden }"
+                v-if="!persistent"
+                @click="closeModal"
+                @keyup.enter="closeModal"
+                ref="closeBtn"
+              >
+                <PhX size="34" />
+                <template #tooltip-text>
+                  <span>Zamknij</span>
+                </template>
+              </IconButton>
+
+              <div
+                class="bottom-controls"
+                :class="{ hidden: controlsHidden }"
+              >
+                <IconButton
+                  v-if="isFullscreen"
+                  class="expand-btn"
+                  @click="toggleObjectFit"
+                  @keyup.enter="toggleObjectFit"
+                  :tooltipPosition="TooltipPosition.top"
+                  ref="expandBtn"
+                >
+                  <PhFrameCorners size="34" />
+                  <template #tooltip-text>
+                    <span>{{ expandBtnText }}</span>
+                  </template>
+                </IconButton>
+
+                <IconButton
+                  class="fullscreen-btn"
+                  @click="toggleFullscreen"
+                  @keyup.enter="toggleFullscreen"
+                  :tooltipPosition="TooltipPosition.top"
+                  ref="fullscreenBtn"
+                >
+                  <PhCornersOut size="34" />
+                  <template #tooltip-text>
+                    <span>Pełny ekran</span>
+                  </template>
+                </IconButton>
+              </div>
+              <div
+                class="body"
+                :class="{
+                  'fullscreen-enabled': isFullscreen,
+                  contain: objectFit === ObjectFit.contain,
+                  cover: objectFit === ObjectFit.cover,
+                }"
+              >
+                <slot />
+              </div>
             </div>
-            <PhX
-              class="close-icon"
-              size="34"
-              v-if="!persistent"
-              @click="closeModal"
-              @keyup.enter="closeModal"
-            />
-          </div>
-          <div
-            class="body"
-            v-if="fullscreen"
-          >
-            <slot />
-          </div>
-          <div
-            class="body"
-            v-else
-          >
-            <n-scrollbar trigger="none">
-              <slot />
-            </n-scrollbar>
-          </div>
+          </template>
+
+          <template v-else>
+            <div class="header">
+              <div class="title">
+                <slot name="title" />
+              </div>
+              <IconButton
+                class="close-btn"
+                v-if="!persistent"
+                @click="closeModal"
+                @keyup.enter="closeModal"
+              >
+                <PhX size="34" />
+                <template #tooltip-text>
+                  <span>Zamknij</span>
+                </template>
+              </IconButton>
+            </div>
+            <div class="body">
+              <n-scrollbar trigger="none">
+                <slot />
+              </n-scrollbar>
+            </div>
+          </template>
         </div>
       </div>
     </Transition>
@@ -151,6 +335,24 @@ watch(
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+}
+
+.fullscreen .modal-content .body.fullscreen-enabled {
+  * {
+    border-radius: 0;
+  }
+
+  &.cover {
+    * {
+      object-fit: cover;
+    }
+  }
+
+  &.contain {
+    * {
+      object-fit: contain;
+    }
   }
 }
 </style>
@@ -216,10 +418,44 @@ watch(
       overflow: visible;
       align-items: flex-end;
       max-width: 95vw;
+      grid-template-rows: auto;
 
-      .body {
-        padding: 0;
-        object-fit: cover;
+      .fullscreen-wrapper {
+        height: 100%;
+        position: relative;
+        overflow: hidden;
+
+        .bottom-controls,
+        .close-btn {
+          transition: transform $transition-duration;
+          position: absolute;
+          right: 1.25rem;
+        }
+
+        .bottom-controls {
+          bottom: 0.75rem;
+          display: flex;
+          gap: 0.5rem;
+
+          &.hidden {
+            transform: translateY(calc(100% + 0.75rem));
+          }
+        }
+
+        .close-btn {
+          top: 0.75rem;
+
+          &.hidden {
+            transform: translateY(calc(-100% - 0.75rem));
+          }
+        }
+        .body {
+          padding: 0;
+
+          &.fullscreen-enabled {
+            max-height: 100vh;
+          }
+        }
       }
     }
   }
@@ -242,7 +478,7 @@ watch(
     height: 95vh;
     overflow: auto;
     display: grid;
-    grid-template-rows: auto auto;
+    grid-template-rows: 60px auto;
 
     &:focus {
       border: none;
@@ -257,11 +493,12 @@ watch(
     }
 
     .header {
+      height: 100%;
       display: grid;
       grid-template-columns: auto 34px;
       justify-items: center;
       align-items: center;
-      padding: 0.5rem 1rem;
+      padding: 0rem 1rem;
       gap: 1rem;
       align-self: flex-start;
 
@@ -273,25 +510,15 @@ watch(
         justify-content: center;
       }
 
-      .close-icon {
-        font-size: 1.5rem;
-        cursor: pointer;
-        border-radius: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: rgba(var(--secondary-color), 0.6);
-        transition: color $transition-duration;
-
-        &:hover {
-          color: rgba(var(--secondary-color), 1);
-        }
+      .close-btn {
+        background-color: transparent;
       }
     }
 
     .body {
-      padding: 0.5rem;
-      height: 88vh;
+      height: 100%;
+      max-height: 95vh;
+      padding: 0rem 0.5rem 0.5rem 0.5rem;
       background-color: $background-color;
     }
   }
